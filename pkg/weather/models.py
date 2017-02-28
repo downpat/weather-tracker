d8d0bda9326a5754c9fc6bd5adebc7eaf0616cbc
Patrick Dobson <pat@downpatproductions.com>
2017-02-27 21:26:34 -0500

    Added first model to weather app

--- pkg/weather/models.py
+++ pkg/weather/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class City(models.Model):
+    name = models.CharField(max_length=200)
+    lat = models.DecimalField(max_digits=12, decimal_places=8)
+    lon = models.DecimalField(max_digits=12, decimal_places=8)
+    country_code = models.CharField(max_length=2)
+    open_weather_id = models.IntegerField()

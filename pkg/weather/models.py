31881844965eb3cd5b058a37d1191ea882b9f5cc
Patrick Dobson <pat@downpatproductions.com>
2017-02-28 21:03:43 -0500

    Defined all models. Created a migration

--- pkg/weather/models.py
+++ pkg/weather/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class City(models.Model):
     name = models.CharField(max_length=200)
@@ -6,3 +7,34 @@ class City(models.Model):
     lon = models.DecimalField(max_digits=12, decimal_places=8)
     country_code = models.CharField(max_length=2)
     open_weather_id = models.IntegerField()
+    users = models.ManyToManyField(User, related_name='cities', through='UserCity')
+
+class UserCity(models.Model):
+    city = models.ForeignKey(City)
+    user = models.ForeignKey(User)
+
+class TempRange(models.Model):
+    email_notification = models.BooleanField(default=False)
+    max_temp = models.DecimalField(max_digits=7, decimal_places=3)
+    min_temp = models.DecimalField(max_digits=7, decimal_places=3)
+    user_city = models.ForeignKey(UserCity)
+
+class Weather(models.Model):
+    city = models.ForeignKey(City)
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    temp = models.DecimalField(max_digits=7, decimal_places=3)
+    max_temp = models.DecimalField(max_digits=7, decimal_places=3)
+    min_temp = models.DecimalField(max_digits=7, decimal_places=3)
+    pressure = models.DecimalField(max_digits=9, decimal_places=3)
+    wind_speed = models.DecimalField(max_digits=7, decimal_places=3)
+    wind_direction = models.DecimalField(max_digits=7, decimal_places=3)
+    clouds = models.DecimalField(max_digits=5, decimal_places=2)
+    calculated = models.DateTimeField()
+    retrieved = models.DateTimeField(auto_now_add=True)
+    
+class Notification(models.Model):
+    weather = models.ForeignKey(Weather)
+    temp_range = models.ForeignKey(TempRange)
+    user = models.ForeignKey(User)
+    unread = models.BooleanField(default=True)
